7759d397fd50b6d1e009b99348ba6d33e3d08c33
Niag2021 <arancibia.guerrero.nicolas@gmail.com>
2023-06-02 18:52:49 -0400

    Mostrar 1 bebida en el servidor.

--- server/controllers/BebidaController.js
+++ server/controllers/BebidaController.js
@@ -18,3 +18,20 @@ export const obtenerBebidas = async (req, res) => {
     }
 }
 
+//Mostrar un registro. 
+export const obtenerBebida = async (req, res) => {
+    //Try catch es para manejar que si ocurre algun error, no se caera el servidor. 
+    try {
+        //Se crea una constante asincrona que retornara el registro que coincide con el id correspondiente al registro 
+        //todo lo que encuentre en BebidaModel, almacenandolo en la constante bebida. 
+        const bebida = await BebidaModel.findAll({
+            where: { id: req.params.id }
+        })
+        //Responde en formato JSON con la bebida que coincida con el id correspondiente. 
+        res.json(bebida[0])
+    } catch (error) {
+        //En caso de error mostrara un mensaje indicando el error. 
+        res.json({ message: error.message })
+    }
+}
+
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -2,11 +2,12 @@
 import express from 'express'
 
 //Se importa el controlador correspondiente a bebidas. 
-import { obtenerBebidas } from "../controllers/BebidaController.js"
+import { obtenerBebidas, obtenerBebida } from "../controllers/BebidaController.js"
 
 const router = express.Router()
     //Se definen los endpoints y sus rutas. 
     router.get('/', obtenerBebidas); 
+    router.get('/:id', obtenerBebida); 
     
 
 export default router
